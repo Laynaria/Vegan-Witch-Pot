c4768ea4100bcfc473d3adca176ee3a086bf4843
Laynaria <pro.laynaria@outlook.fr>
2023-02-01 16:37:19 +0100

    added recipes pages, and did some fixes here and there

---
+++ frontend/src/components/Home/Recipes.jsx
@@ -0,0 +1,10 @@
+import "./Recipes.scss";
+
+export default function Recipes() {
+  return (
+    <section className="Recipes">
+      <p>Reicipes</p>
+      <p>wink wink</p>
+    </section>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Landing from "@components/Home/Landing";
+import Recipes from "@components/Home/Recipes";
 
 export default function Home() {
   const [isLanding, setIsLanding] = useState(true);
   return (
     <main>
-      {isLanding ? <Landing setIsLanding={setIsLanding} /> : <p>HomePage</p>}
+      {isLanding ? <Landing setIsLanding={setIsLanding} /> : <Recipes />}
     </main>
   );
 }
