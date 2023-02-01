71f107986daeecf2cd607f684d37df90bd0af96e
Laynaria <pro.laynaria@outlook.fr>
2023-02-01 14:43:57 +0100

    made variables, and font imports now working

---
+++ frontend/src/components/Home/Landing.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <header className="App-header">
+      <p>Landing</p>
+      <button type="button">Enter</button>
+    </header>
+  );
+}
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
+import Error from "@pages/Error";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
 
-        {/* <Route path="*" element={<Error />} /> */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
