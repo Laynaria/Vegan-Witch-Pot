3c1edc00b97de48fc39f710d5d6209ea921a27f0
Laynaria <pro.laynaria@outlook.fr>
2023-02-02 12:57:19 +0100

    forgot to apply good link for picture

--- frontend/src/components/Home/Recipes.jsx
+++ frontend/src/components/Home/Recipes.jsx
@@ -3,146 +3,11 @@ import instance from "@services/instance";
 
 import spoon from "@assets/icons/spoon.svg";
 import timeImg from "@assets/logos/logo_mini.svg";
-import grilledPeas from "@assets/recipes/mini/grilled_peas.png";
 
 import "./Recipes.scss";
 
 export default function Recipes() {
-  const [arrayRecipes, setArrayRecipes] = useState([
-    {
-      id: 1,
-      title: "Grilled Peas",
-      desc: "description",
-      difficulty: "1",
-      time: "30min",
-    },
-    {
-      id: 2,
-      title: "Recette",
-      desc: "description",
-      difficulty: "2",
-      time: "10min",
-    },
-    {
-      id: 3,
-      title: "Recette",
-      desc: "description",
-      difficulty: "3",
-      time: "15min",
-    },
-    {
-      id: 4,
-      title: "Recette",
-      desc: "description",
-      difficulty: "4",
-      time: "20min",
-    },
-    {
-      id: 5,
-      title: "Recette",
-      desc: "description",
-      difficulty: "5",
-      time: "30min",
-    },
-    {
-      id: 6,
-      title: "Recette",
-      desc: "description",
-      difficulty: "5",
-      time: "25min",
-    },
-    {
-      id: 7,
-      title: "Recette",
-      desc: "description",
-      difficulty: "2",
-      time: "75min",
-    },
-    {
-      id: 8,
-      title: "Recette",
-      desc: "description",
-      difficulty: "4",
-      time: "25min",
-    },
-    {
-      id: 9,
-      title: "Recette",
-      desc: "description",
-      difficulty: "3",
-      time: "1h",
-    },
-    {
-      id: 10,
-      title: "Recette",
-      desc: "description",
-      difficulty: "2",
-      time: "1h",
-    },
-    {
-      id: 11,
-      title: "Five Star",
-      desc: "description",
-      difficulty: "5",
-      time: "3h",
-    },
-    {
-      id: 12,
-      title: "Recette",
-      desc: "description",
-      difficulty: "1",
-      time: "4h",
-    },
-    {
-      id: 13,
-      title: "Two Star",
-      desc: "description",
-      difficulty: "2",
-      time: "24h",
-    },
-    {
-      id: 14,
-      title: "Recette",
-      desc: "description",
-      difficulty: "3",
-      time: "48h",
-    },
-    {
-      id: 15,
-      title: "Recette",
-      desc: "description",
-      difficulty: "1",
-      time: "1h",
-    },
-    {
-      id: 16,
-      title: "Recette",
-      desc: "description",
-      difficulty: "1",
-      time: "1h30",
-    },
-    {
-      id: 17,
-      title: "Four Star",
-      desc: "description",
-      difficulty: "4",
-      time: "7h",
-    },
-    {
-      id: 18,
-      title: "Three Star",
-      desc: "description",
-      difficulty: "3",
-      time: "22min",
-    },
-    {
-      id: 19,
-      title: "One Star",
-      desc: "description",
-      difficulty: "1",
-      time: "1h45",
-    },
-  ]);
+  const [arrayRecipes, setArrayRecipes] = useState([]);
 
   useEffect(() => {
     instance
@@ -161,7 +26,11 @@ export default function Recipes() {
       <h1>Recipes</h1>
       {arrayRecipes.map((recipe) => (
         <div className="card" key={recipe.id}>
-          <img src={grilledPeas} alt={recipe.title} className="card-img" />
+          <img
+            src={`http://localhost:3000/src/assets/recipes/mini/${recipe.picture}`}
+            alt={recipe.title}
+            className="card-img"
+          />
           <h2>{recipe.title}</h2>
           <p>
             <span>
