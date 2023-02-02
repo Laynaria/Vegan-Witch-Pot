e9f8182e1ccef4e4a326d6abc0476f0d5aa7b7a7
Laynaria <pro.laynaria@outlook.fr>
2023-02-02 12:28:47 +0100

    get all recipe route working in the backend

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const recipeControllers = require("./controllers/recipeControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/recipes", recipeControllers.browse);
 
 module.exports = router;
