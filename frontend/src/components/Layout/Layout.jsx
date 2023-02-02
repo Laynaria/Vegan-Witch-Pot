558b86c4054f06a4dc6529b1d63971e494973c57
Laynaria <pro.laynaria@outlook.fr>
2023-02-02 09:55:24 +0100

    made layout route, so all the other compoents are always inside the route

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main>
+        {children}
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
+
+import Layout from "@components/Layout/Layout";
 import Error from "@pages/Error";
+import Recipes from "@components/Home/Recipes";
 
 export default function Router() {
   return (
@@ -8,8 +11,10 @@ export default function Router() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/" element={<Home />} />
-
-        <Route path="*" element={<Error />} />
+        <Route element={<Layout />}>
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
