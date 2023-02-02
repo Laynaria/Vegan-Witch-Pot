309306e110f849a4021751c88001a739d86801bb
Laynaria <pro.laynaria@outlook.fr>
2023-02-02 17:14:37 +0100

    some little changes, mainly of input types

--- frontend/src/pages/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe.jsx
@@ -23,15 +23,26 @@ export default function AddRecipe() {
       <section className="edit">
         <form onChange={handleChange}>
           <label>
+            Title
             <input type="text" name="title" value={recipe.title} />
           </label>
           <label>
+            Picture Link
+            {/* should maybe change it to an input type link */}
             <input type="text" name="picture" value={recipe.picture} />
           </label>
           <label>
-            <input type="number" name="difficulty" value={recipe.difficulty} />
+            Difficulty
+            <input
+              type="range"
+              name="difficulty"
+              value={recipe.difficulty}
+              min="1"
+              max="5"
+            />
           </label>
           <label>
+            Cooking Time
             <input
               type="text"
               name="cooking_time"
