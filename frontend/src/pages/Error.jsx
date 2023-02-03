7acfcc007f8ac4f780c66e647e07ee7b779151fd
Laynaria <pro.laynaria@outlook.fr>
2023-02-03 16:03:20 +0100

    error 404 page done, also added an animation for future loading component when we wait for backend info to load

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,15 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import avocado from "@assets/lotties/avocado_error.json";
+
 export default function Error() {
   return (
     <main>
-      <p>Error</p>
+      <Player autoplay loop src={avocado} className="Avocado Error 404" />
+      <p>You lost your way, like this little magic avocado.</p>
+      <p>
+        You should maybe think about getting back to somewhere the witch wants
+        you...
+      </p>
     </main>
   );
 }
