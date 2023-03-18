094e50858fbbdcdcdc489458aab8b83ce38afb91
Laynaria <pro.laynaria@outlook.fr>
2023-03-18 20:43:43 +0100

    little change for landing component to appear only on refresh of pages for now, next step is to do it with a session storage instead

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-import Landing from "@components/Home/Landing";
 import Layout from "@components/Layout/Layout";
 import Recipes from "@components/Home/Recipes";
 
 export default function Home() {
-  const [isLanding, setIsLanding] = useState(true);
   return (
-    <>
-      {isLanding ? (
-        <main>
-          <Landing setIsLanding={setIsLanding} />
-        </main>
-      ) : (
-        <Layout>
-          <Recipes />
-        </Layout>
-      )}
-    </>
+    <Layout>
+      <Recipes />
+    </Layout>
   );
 }
