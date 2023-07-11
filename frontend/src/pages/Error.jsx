abc3556a1fe8ab21df9a5e92ac9b644c74aa1d72
Laynaria <pro.laynaria@outlook.fr>
2023-07-11 17:28:40 +0200

    improved responsive of loading component and error 404 page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -4,9 +4,13 @@ import avocado from "@assets/lotties/avocado_error.json";
 export default function Error() {
   return (
     <>
-      <Player autoplay loop src={avocado} />
-      <p>You lost your way, like this little magic avocado.</p>
-      <p>
+      <Player autoplay loop src={avocado} style={{ maxWidth: "95vw" }} />
+      <p style={{ maxWidth: "95vw", textAlign: "center" }}>
+        You lost your way, like this little magic avocado.
+      </p>
+      <p
+        style={{ maxWidth: "95vw", marginBottom: "4rem", textAlign: "center" }}
+      >
         You should maybe think about getting back to somewhere the witch wants
         you...
       </p>
