391536bf3f9ae62683ba446e1287537835564c2f
Laynaria <pro.laynaria@outlook.fr>
2023-07-18 19:35:29 +0200

    CookiesPopUp component is done, it now advertiste that we use functionnal cookies

---
+++ frontend/src/components/CookiesPopUp/CookiesPopUp.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+import "./CookiesPopUp.scss";
+
+export default function Error({ setIsCookiesPopUpShown }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleCookiesPopUp = () => {
+    setIsCookiesPopUpShown(false);
+    localStorage.setItem("cookies_pop_up", JSON.stringify({ state: false }));
+  };
+
+  useEffect(() => {
+    const getPopUpInfo = JSON.parse(localStorage.getItem("cookies_pop_up"));
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      if (getPopUpInfo && getPopUpInfo.state === false) {
+        setIsCookiesPopUpShown(false);
+      }
+    }, 300);
+  }, []);
+
+  return (
+    <section className={!isLoading ? "CookiesPopUp" : "CookiesPopUp hide"}>
+      <p>
+        We use some cookies crumbs for your magic experience. Only functionnal
+        ones of course.
+      </p>
+      <button
+        type="button"
+        className={!isLoading ? "" : "hide"}
+        onClick={handleCookiesPopUp}
+      >
+        Munch It!
+      </button>
+    </section>
+  );
+}
