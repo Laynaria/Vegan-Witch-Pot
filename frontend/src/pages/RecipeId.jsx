54ec87120f395318996dbf305a4d43f4ab8ddc70
Laynaria <pro.laynaria@outlook.fr>
2023-08-07 19:06:02 +0200

    if a recipe isn't approved and isn't ours, we shouldn't be able to access it anymore

--- frontend/src/pages/RecipeId.jsx
+++ frontend/src/pages/RecipeId.jsx
@@ -1,20 +1,28 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import instance from "@services/instance";
 import Loading from "@components/Loading/Loading";
 import Card from "@components/Card/Card";
+import { AuthContext } from "@contexts/AuthContext";
 
 export default function RecipeId() {
   const [recipe, setRecipe] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
 
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     setTimeout(() => {
       instance
         .get(`/recipes/${id}`)
         .then((result) => {
-          setRecipe(result.data);
+          if (result.user_id !== user.id && !result.is_approved) {
+            return navigate("/");
+          }
+          return setRecipe(result.data);
         })
         .then(() => setIsLoading(false))
         .catch((err) => {
