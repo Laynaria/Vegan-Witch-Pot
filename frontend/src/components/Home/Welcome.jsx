b5a69902d9c1c3bdc090e9d3a09ff070d8ae5d20
Laynaria <pro.laynaria@outlook.fr>
2023-08-09 17:05:59 +0200

    improved home page with a new welcome component

---
+++ frontend/src/components/Home/Welcome.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import cat from "@assets/lotties/cat_welcome.json";
+
+import "@components/Home/Welcome.scss";
+
+export default function Welcome() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 100);
+  }, []);
+  return (
+    <section className="Welcome">
+      <h1 className={isLoading ? "hide" : ""}>Welcome to Vegan Witch Pot</h1>
+      <Player
+        autoplay
+        loop
+        src={cat}
+        className={isLoading ? "hide" : ""}
+        style={{ transition: "opacity 0.55s ease" }}
+      />
+    </section>
+  );
+}
