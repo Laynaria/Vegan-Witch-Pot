5af33da2be88733de7bc354672a475444f32e8ed
Laynaria <pro.laynaria@outlook.fr>
2023-08-19 18:28:09 +0200

    worked on the why is the witch vegan section content

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -21,8 +21,8 @@ export default function AboutUs() {
         </p>
       </section>
       <section>
-        <h2> </h2>
-        <p> </p>
+        <h2>Who is the Witch?</h2>
+        <p>It's a secret! {"<3"}</p>
       </section>
       <section>
         <h2>Why is the Witch Vegan?</h2>
@@ -33,7 +33,8 @@ export default function AboutUs() {
           className={isLoading ? "hide" : ""}
           style={{ transition: "opacity 1s ease" }}
         />
-        <p>Firstly because</p>
+        <p>Firstly because she doesn't like animals to suffer.</p>
+        <p>But mainly because more animals means more minions to her!</p>
       </section>
     </>
   );
