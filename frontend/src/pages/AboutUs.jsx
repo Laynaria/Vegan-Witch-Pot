4a48882d766d1679a65a1ed823632bf3af970d6e
Laynaria <pro.laynaria@outlook.fr>
2023-08-19 18:24:07 +0200

    added nearly everything needed to work on the about us page

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,17 +1,39 @@
+import { useState, useEffect } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import demon from "@assets/lotties/demon_about_us.json";
+
 export default function AboutUs() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 100);
+  }, []);
+
   return (
     <>
+      <h1>About Us</h1>
       <section>
-        <h1>What is Vegan Witch Pot?</h1>
+        <h2>What is Vegan Witch Pot?</h2>
+        <p>
+          VWP is a community web app revolving around creating and sharing
+          recipes.
+        </p>
+      </section>
+      <section>
+        <h2> </h2>
         <p> </p>
       </section>
       <section>
-        <h1> </h1>
-        <p> </p>
-      </section>
-      <section>
-        <h1>Why is the Witch Vegan?</h1>
-        <p> </p>
+        <h2>Why is the Witch Vegan?</h2>
+        <Player
+          autoplay
+          loop
+          src={demon}
+          className={isLoading ? "hide" : ""}
+          style={{ transition: "opacity 1s ease" }}
+        />
+        <p>Firstly because</p>
       </section>
     </>
   );
