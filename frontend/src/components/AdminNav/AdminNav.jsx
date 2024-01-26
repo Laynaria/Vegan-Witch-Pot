b065cf7ea7d3e4cfb2d314ebb8464c74cceba19d
Laynaria <pro.laynaria@outlook.fr>
2024-01-26 23:08:56 +0100

    prepared the base of the four buttons and did a first try of unworking animation

--- frontend/src/components/AdminNav/AdminNav.jsx
+++ frontend/src/components/AdminNav/AdminNav.jsx
@@ -1,9 +1,37 @@
+import { Link } from "react-router-dom";
 import "./AdminNav.scss";
+
+import buttonIcon from "@assets/logos/logo_mini.svg";
 
 export default function AdminNav() {
   return (
-    <nav className="AdminNav" style={{ position: "fixed", zIndex: 5000 }}>
-      <p>Admin</p>
+    <nav className="AdminNav">
+      <ul>
+        <Link to="/Admin-Dashboard/Recipes">
+          <li>
+            <img src={buttonIcon} alt="" />
+            <span>Recipes</span>
+          </li>
+        </Link>
+        <Link to="/Admin-Dashboard/Ingredients">
+          <li>
+            <img src={buttonIcon} alt="" />
+            <span>Ingredients</span>
+          </li>
+        </Link>
+        <Link to="/Admin-Dashboard/Users">
+          <li>
+            <img src={buttonIcon} alt="" />
+            <span>Users</span>
+          </li>
+        </Link>
+        <Link to="/Admin-Dashboard/Contacts">
+          <li>
+            <img src={buttonIcon} alt="" />
+            <span>Contacts</span>
+          </li>
+        </Link>
+      </ul>
     </nav>
   );
 }
